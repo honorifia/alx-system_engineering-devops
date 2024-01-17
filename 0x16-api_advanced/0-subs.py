1f3c04493df86f3c33709c71922a1278acdd6f15
honorifica <mkajiwonele@must.ac.mw>
2024-01-17 14:28:53 -0800

    api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/DiligentRabbit1261)"
+    }
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+
+        if response.status_code == 200:
+            results = response.json().get("data")
+            return results.get("subscribers", 0)
+        else:
+            return 0
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return 0
