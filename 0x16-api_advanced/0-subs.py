f0054e2e724e63db8730c782ed295492517cd64e
honorifica <mkajiwonele@must.ac.mw>
2024-01-17 14:32:31 -0800

    update task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/DiligentRabbit1261)"
+        "User-Agent": "linux:0x16.api.advanc:v1.0.0 (by /u/DiligentRabbit1261)"
     }
 
     try:
